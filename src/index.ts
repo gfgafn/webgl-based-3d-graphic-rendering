3434ed8003ad3187e7fb476dc8187dd1bc712b30
gfgafn <87858323+gfgafn@users.noreply.github.com>
2022-08-22 23:45:15 +0800

    refactor: 调整代码结构

--- src/index.ts
+++ src/index.ts
@@ -16,82 +16,16 @@ const canvas: HTMLCanvasElement | null = document.getElementById(
     'webgl',
 ) as HTMLCanvasElement;
 
-const demoSelectOptions: string[] = [
-    '第3章: RotatingCubeApplication',
-    '第3章: AsyncLoadTestApplication',
-    '第4章: BasicWebGLApplication',
-    '第7章: MeshBuilderApplication',
-    '第7章: CoordSystemApplication',
-    '第8章: Q3BspApplication',
-    '第9章: Doom3Application',
-    '第10章: MD5SkinedMeshApplication',
-];
-
-const demoApplications = [
-    RotatingCubeApplication,
-    AsyncLoadTestApplication,
-    BasicWebGLApplication,
-    MeshBuilderApplication,
-    CoordSystemApplication,
-    Q3BspApplication,
-    Doom3Application,
-    MD5SkinedMeshApplication,
-];
-
-function addOptionsToSelectElement(select: HTMLSelectElement): void {
-    if (!canvas) return;
-    demoSelectOptions.forEach((option) => select.options.add(new Option(option, option)));
-}
-
-enum EAPPName {
-    ROTATINGCUBE,
-    ASYNCLOAD,
-    BASICWEBGL,
-    MESHBUILDER,
-    COORDSYSTEM,
-    Q3BSP,
-    DOOM3PROC,
-    DOOM3MD5,
-}
-
-function runApplication(name: EAPPName): void {
-    // 获取用于获得webgl上下文对象的HTMLCanvasElement元素
-    const canvas: HTMLCanvasElement | null = document.getElementById(
-        'webgl',
-    ) as HTMLCanvasElement;
-    if (name === EAPPName.ROTATINGCUBE) {
-        const app: RotatingCubeApplication = new RotatingCubeApplication(canvas);
-        app.frameCallback = frameCallback;
-        app.run();
-    } else if (name === EAPPName.ASYNCLOAD) {
-        const app: AsyncLoadTestApplication = new AsyncLoadTestApplication(canvas);
-        app.run();
-    } else if (name === EAPPName.BASICWEBGL) {
-        const app: Application = new BasicWebGLApplication(canvas);
-        app.frameCallback = frameCallback;
-        app.run();
-    } else if (name === EAPPName.MESHBUILDER) {
-        const app: Application = new MeshBuilderApplication(canvas);
-        app.frameCallback = frameCallback;
-        app.start();
-    } else if (name === EAPPName.COORDSYSTEM) {
-        const app: CoordSystemApplication = new CoordSystemApplication(canvas);
-        app.frameCallback = frameCallback;
-        app.run();
-    } else if (name === EAPPName.Q3BSP) {
-        const app: Application = new Q3BspApplication(canvas);
-        app.frameCallback = frameCallback;
-        app.run();
-    } else if (name === EAPPName.DOOM3PROC) {
-        const app: Doom3Application = new Doom3Application(canvas);
-        app.frameCallback = frameCallback;
-        app.run();
-    } else if (name === EAPPName.DOOM3MD5) {
-        const app: MD5SkinedMeshApplication = new MD5SkinedMeshApplication(canvas);
-        app.frameCallback = frameCallback;
-        app.run();
-    }
-}
+const demoApplicationsMapper = {
+    '第3章: RotatingCubeApplication': RotatingCubeApplication,
+    '第3章: AsyncLoadTestApplication': AsyncLoadTestApplication,
+    '第4章: BasicWebGLApplication': BasicWebGLApplication,
+    '第7章: MeshBuilderApplication': MeshBuilderApplication,
+    '第7章: CoordSystemApplication': CoordSystemApplication,
+    '第8章: Q3BspApplication': Q3BspApplication,
+    '第9章: Doom3Application': Doom3Application,
+    '第10章: MD5SkinedMeshApplication': MD5SkinedMeshApplication,
+} as const;
 
 function createText(tagName: string): Text {
     const elem: HTMLSpanElement = document.getElementById(tagName) as HTMLSpanElement;
@@ -100,9 +34,9 @@ function createText(tagName: string): Text {
     return text;
 }
 
-const fps: Text = createText('fps');
-const tris: Text = createText('tris');
-const verts: Text = createText('verts');
+const [fps, tris, verts]: Text[] = ['fps', 'tris', 'verts'].map((value) =>
+    createText(value),
+);
 
 function frameCallback(app: Application): void {
     fps.nodeValue = String(app.fps.toFixed(0));
@@ -110,19 +44,33 @@ function frameCallback(app: Application): void {
     verts.nodeValue = '0';
 }
 
-addOptionsToSelectElement(select);
+type demoApplicationNameType = keyof typeof demoApplicationsMapper;
+
+Object.keys(demoApplicationsMapper).forEach((key) =>
+    select.options.add(new Option(key, key)),
+);
 
 select.onchange = () => {
-    const app: Application = new demoApplications[select.selectedIndex](canvas);
+    const appName = select.value as demoApplicationNameType;
+    const app: Application = new demoApplicationsMapper[appName](canvas);
+    runApplication(app);
+};
+
+const runApplication = (
+    app: Application | (new (canvas: HTMLCanvasElement) => Application),
+) => {
+    if (typeof app === 'function') {
+        app = new app(canvas);
+    }
     app.frameCallback = frameCallback;
     app.run();
 };
 
-runApplication(EAPPName.ROTATINGCUBE);
-// runApplication(EAPPName.ASYNCLOAD);
-// runApplication(EAPPName.BASICWEBGL);
-// runApplication(EAPPName.MESHBUILDER);
-// runApplication(EAPPName.COORDSYSTEM);
-// runApplication(EAPPName.Q3BSP);
-// runApplication(EAPPName.DOOM3PROC);
-// runApplication(EAPPName.DOOM3MD5);
+runApplication(RotatingCubeApplication);
+// runApplication(AsyncLoadTestApplication);
+// runApplication(BasicWebGLApplication);
+// runApplication(MeshBuilderApplication);
+// runApplication(CoordSystemApplication);
+// runApplication(Q3BspApplication);
+// runApplication(Doom3Application);
+// runApplication(MD5SkinedMeshApplication);
